52ffcb0079ee8618b588fe65df23fd1d1c4acf90
mrlakdiv <109060995+mrlakdiv@users.noreply.github.com>
2022-07-11 12:19:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,27 +20,27 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is Manuth Lakdiv😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94741213044'] //Owner number in aive msg
+global.premium = ['++94741213044'] //Owner Number info
+global.ownernomer = '+94741213044' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎MR.MANUTH LAKDIV' //Owner Name
+global.botname = ' ☬MS༒BOT࿐' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤭FaceBook📍' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://www.facebook.com/manuth.lkh/' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'SriLanka, Southern Province, Matara' //Country Region
 
 global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.author = '😎MR.Manuth❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©MS_Creations' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
